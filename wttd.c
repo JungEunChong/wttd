1353985080f4be63c0f100191b96d36d32b78175
JungEunChong <21900673@handong.edu>
2020-06-13 21:50:59 +0900

    free - 3

--- wttd.c
+++ wttd.c
@@ -3,6 +3,7 @@
 int main() {
 	int a;
 	int b;
+	int mode;
 	
 	printf("a : ");
 	scanf("%d", &a);
@@ -10,10 +11,15 @@ int main() {
 	printf("b : ");
 	scanf("%d", &b);
 	
-	printf("a + b = %d\n", a+b);
-	printf("a - b = %d\n", a-b);
-	printf("a x b = %d\n", a*b);
-	printf("a / b = %d\n", a/b);
+	printf("mode : ");
+	scanf("%d", &mode);
+
+
+	if(mode == 1) printf("a + b = %d\n", a+b);
+	else if(mode == 2) printf("a - b = %d\n", a-b);
+	else if(mode == 3) printf("a x b = %d\n", a*b);
+	else if(mode == 4) printf("a / b = %d\n", a/b);
+	else printf("please press 1~4\n");
 
 	return 0;
 }
